958f584e82a9855957f09caf0073142d4c975694
Michael Jordan <Insurikai@gmail.com>
2019-11-11 16:27:14 -0800

    Completed MVP

--- README.md
+++ README.md
@@ -51,12 +51,12 @@ The expected/assumed behavior of the application is listed below.
 
 ### Gate
 
-- defaults to `unlocked` and `open`
-- cannot be closed or opened if it is locked
+- defaults to `unlocked` and `open`*
+- cannot be closed or opened if it is locked*
 
 ### Dashboard
 
-- shows the controls and display
+- shows the controls and display*
 
 ### Display Component
 
--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,41 @@
-// Test away!
+// Test away
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from '../dashboard/Dashboard';
+
+describe('Dashboard',()=>  {
+  it('cannot be closed or opened if it is locked', async ()=>{
+    const wrapper = rtl.render(<Dashboard />);
+    const closeOpenButton = await wrapper.findByText('Close Gate');
+    const lockUnlockButton = await wrapper.findByText('Lock Gate');
+    closeOpenButton.click();
+    lockUnlockButton.click();
+    expect(closeOpenButton.hasAttribute('disabled'))
+    rtl.cleanup();
+  })
+  it('provide buttons to toggle the `closed` and `locked` states.', ()=>{
+    const wrapper = rtl.render(<Dashboard />);
+    
+    rtl.cleanup();
+  })
+  it('buttons text changes to reflect the state the door will be in if clicked', ()=>{
+    const wrapper = rtl.render(<Dashboard />);
+    rtl.cleanup();
+  })
+  it('the closed toggle button is disabled if the gate is locked', ()=>{
+    const wrapper = rtl.render(<Dashboard />);
+    rtl.cleanup();
+  })
+  it('the locked toggle button is disabled if the gate is open', ()=>{
+    const wrapper = rtl.render(<Dashboard />);
+    rtl.cleanup();
+  })
+})
+
+/*
+- provide buttons to toggle the `closed` and `locked` states.
+- buttons' text changes to reflect the state the door will be in if clicked
+- the closed toggle button is disabled if the gate is locked
+- the locked toggle button is disabled if the gate is open  
+*/
