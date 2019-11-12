1c9aa160d73adc767b55dc685fc1a2d49712d081
Michael Jordan <Insurikai@gmail.com>
2019-11-12 09:32:25 -0800

    Finished

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -4,7 +4,7 @@ import * as rtl from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import Dashboard from '../dashboard/Dashboard';
 
-describe('Dashboard',()=>  {
+describe('Controls',()=>  {
   it('cannot be closed or opened if it is locked', async ()=>{
     const wrapper = rtl.render(<Dashboard />);
     const closeOpenButton = await wrapper.findByText('Close Gate');
@@ -16,19 +16,33 @@ describe('Dashboard',()=>  {
   })
   it('provide buttons to toggle the `closed` and `locked` states.', ()=>{
     const wrapper = rtl.render(<Dashboard />);
-    
+    expect(wrapper.findByText('Close Gate'))
+    expect(wrapper.findByText('Lock Gate'));
     rtl.cleanup();
   })
-  it('buttons text changes to reflect the state the door will be in if clicked', ()=>{
+  it('buttons text changes to reflect the state the door will be in if clicked', async ()=>{
     const wrapper = rtl.render(<Dashboard />);
+    const closeOpenButton = await wrapper.findByText('Close Gate');
+    closeOpenButton.click();
+    expect(closeOpenButton.textContent === 'Open Gate');
+    const lockUnlockButton = await wrapper.findByText('Lock Gate');
+    lockUnlockButton.click();
+    expect(lockUnlockButton.textContent === 'Unlock Gate');
     rtl.cleanup();
   })
-  it('the closed toggle button is disabled if the gate is locked', ()=>{
+  it('the closed toggle button is disabled if the gate is locked', async ()=>{
     const wrapper = rtl.render(<Dashboard />);
+    const closeOpenButton = await wrapper.findByText('Close Gate');
+    closeOpenButton.click();
+    const lockUnlockButton = await wrapper.findByText('Lock Gate');
+    lockUnlockButton.click();
+    expect(closeOpenButton.hasAttribute('disabled'))
     rtl.cleanup();
   })
-  it('the locked toggle button is disabled if the gate is open', ()=>{
+  it('the locked toggle button is disabled if the gate is open', async ()=>{
     const wrapper = rtl.render(<Dashboard />);
+    const lockButton = await wrapper.findByText('Lock Gate');
+    expect(lockButton.hasAttribute('disabled'));
     rtl.cleanup();
   })
 })
